55da1b13ca72bf3600249dc4f7fc5f72b0c46f81
Tanner Rhines <tannerrhines@protonmail.com>
2023-06-17 03:05:33 -0800

    hourly color/ refresh color functions

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,29 @@ console.log("the files are linked");
 const currentHour = dayjs().format('H');
 
 
+function hourlyColor() {
+  $('.time-block').each(function() {
+    const blockHour = parseInt(this.id);
+    $(this).toggleClass('past', blockHour < currentHour);
+    $(this).toggleClass('present', blockHour === currentHour);
+    $(this).toggleClass('future', blockHour > currentHour);
+  });
+}
+
+function refreshColor() {
+  $('.time-block').each(function() {
+    const blockHour = parseInt(this.id);
+    if (blockHour == currentHour) {
+      $(this).removeClass('past future').addClass('present');
+    } else if (blockHour < currentHour) {
+      $(this).removeClass('future present').addClass('past');
+    } else {
+      $(this).removeClass('past present').addClass('future');
+    }
+  });
+}
+
+
 
 
 
@@ -25,6 +48,12 @@ function currentTime() {
 
 setInterval(currentTime, 1000);
 
+
+
+
+currentTime();
+hourlyColor();
+refreshColor();
 
 
 
