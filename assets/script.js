cb1dde9231769bd9d2b5163691273130e8242623
Tanner Rhines <tannerrhines@protonmail.com>
2023-06-17 02:57:28 -0800

    created function to set the current Time and Region

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,24 @@ console.log("the files are linked");
 
 
 
+
+
+
+
+
+
+
+
+// function to set the current Time and Region 
+function currentTime() {
+  var dateElement = $('#date');
+  var current = new Date();
+  dateElement.text(current);
+}
+
+setInterval(currentTime, 1000);
+
+
+
+
+
