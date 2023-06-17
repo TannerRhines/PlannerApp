a5f354c86121cdde672272c1b3933e26549ee5d7
Tanner Rhines <tannerrhines@protonmail.com>
2023-06-17 03:26:05 -0800

    added text area css

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,7 @@
 console.log("the files are linked");
 
-
-
-
 // declare current hour using dayJS
-const currentHour = dayjs().format('H');
-
-
-
+let currentHour = dayjs().format('H');
 
 function enterText() {
   $('.saveBtn').on('click', function() {
@@ -17,57 +11,43 @@ function enterText() {
   });
 }
 
-
-
 $('.time-block').each(function() {
   const key = $(this).attr('id');
   const value = localStorage.getItem(key);
   $(this).children('.description').val(value);
 });
 
-
+// The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
 function hourlyColor() {
   $('.time-block').each(function() {
     const blockHour = parseInt(this.id);
     $(this).toggleClass('past', blockHour < currentHour);
-    $(this).toggleClass('present', blockHour === currentHour);
+    $(this).toggleClass('present', blockHour == currentHour);
     $(this).toggleClass('future', blockHour > currentHour);
   });
 }
 
-function refreshColor() {
-  $('.time-block').each(function() {
-    const blockHour = parseInt(this.id);
-    if (blockHour == currentHour) {
-      $(this).removeClass('past future').addClass('present');
-    } else if (blockHour < currentHour) {
-      $(this).removeClass('future present').addClass('past');
-    } else {
-      $(this).removeClass('past present').addClass('future');
-    }
+// The function below will save the user's input in a textarea to localStorage - only when the corresponding save button has been clicked.
+function textEntry() {
+  $('.saveBtn').on('click', function() {
+    const key = $(this).parent().attr('id');
+    const value = $(this).siblings('.description').val();
+    localStorage.setItem(key, value);
   });
 }
-
-
-
-
 
 // function to set the current Time and Region 
 function currentTime() {
   var dateElement = $('#date');
   var current = new Date();
   dateElement.text(current);
+
+  // Update the currentHour every time currentTime is called.
+  currentHour = dayjs().format('H');
+  hourlyColor();
 }
 
 setInterval(currentTime, 1000);
 
-
 enterText();
-
 currentTime();
-hourlyColor();
-refreshColor();
-
-
-
-
